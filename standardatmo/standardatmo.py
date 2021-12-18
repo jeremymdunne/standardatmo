4c24a0d531010a960dee263b8bb879e31bd7c247
Jeremy Dunne <jeremymdunne@gmail.com>
2021-12-18 14:35:50 -0800

    first commit

---
+++ standardatmo/standardatmo.py
@@ -0,0 +1,89 @@
+# code to load in the atmospheric file
+from os import path # for relative path import
+import csv # for atmo data reading
+
+
+
+######### CONSTANTS ##########
+ATMO_ALTITUDE_INDEX = 0
+ATMO_TEMPERATURE_INDEX = 1
+ATMO_GRAVITY_INDEX = 2
+ATMO_PRESSURE_INDEX = 3
+ATMO_DENSITY_INDEX = 4
+ATMO_VISCOSITY_INDEX = 5
+
+
+########### CODE ##########
+def loadAtmoData():
+    basepath = path.dirname(__file__)
+    filepath = path.abspath(path.join(basepath, "..", "resources", "standard_atmospheric_data.txt"))
+    atmodata = {
+    "altitude":[], "temperature": [],"gravity": [],
+    "pressure": [],"density": [],"viscosity": []
+    }
+    with open(filepath, newline='') as atmofile:
+        atmoreader = csv.reader(atmofile, delimiter=',')
+        for row in atmoreader:
+            try:
+                linedata = []
+                for i in row:
+                    linedata.append(float(i))
+                atmodata["altitude"].append(linedata[ATMO_ALTITUDE_INDEX])
+                atmodata["temperature"].append(linedata[ATMO_TEMPERATURE_INDEX])
+                atmodata["gravity"].append(linedata[ATMO_GRAVITY_INDEX])
+                atmodata["pressure"].append(linedata[ATMO_PRESSURE_INDEX])
+                atmodata["density"].append(linedata[ATMO_DENSITY_INDEX])
+                atmodata["viscosity"].append(linedata[ATMO_VISCOSITY_INDEX])
+            except:
+                pass
+    atmofile.close()
+
+def getAtmoData(altitude):
+    # create a named dictionary of the data
+    dict = {
+        "altitude":altitude, "temperature": None,"gravity": None,
+        "pressure": None,"density": None,"viscosity": None
+        }
+    # interpolate for each data set
+    for key in dict.keys():
+        if key != 'altitude':
+            dict[key] = atmoInterpolate(altitude, key)
+    return dict
+
+def getTemperatureData(altitude):
+    return atmoInterpolate(altitude, 'temperature')
+
+def getGravityData(altitude):
+    return atmoInterpolate(altitude, 'gravity')
+
+def getPressureData(altitude):
+    return atmoInterpolate(altitude, 'pressure')
+
+def getDensityData(altitude):
+    return atmoInterpolate(altitude, 'density')
+
+def getViscosityData(altitude):
+    return atmoInterpolate(altitude, 'viscosity')
+
+def atmoInterpolate(altitude, key):
+    # linear interpolation
+    # find closest index
+    y_data = atmodata[key]
+    x_data = atmodata['altitude']
+    n = 0
+    if (altitude < x_data[0]) or (altitude > x_data[-1]):
+        print("WARNING! standardatmo DATA SET OUT OF BOUNDS, INTERPOLATING PAST KNOWN DATA")
+    for i in range(0, len(x_data)):
+        if x_data[i] < altitude:
+            n = i
+            break
+    # linear
+    y_inter = (y_data[n] * (x_data[n+1] - altitude) + y_data[n+1] * (altitude - x_data[n])) / (x_data[n+1] - x_data[n])
+    return y_inter
+
+
+
+loadAtmoData() 
+
+if __name__ == '__main__':
+    getAtmoData(-1200)
---
+++ standardatmo_test.py
@@ -0,0 +1,27 @@
+import standardatmo
+import matplotlib.pyplot as plt
+
+# get atmo data from -1000 to 80000 m
+altitude_data = []
+pressure_data = []
+temperature_data = []
+gravity_data = []
+density_data = []
+viscosity_data = []
+
+for h in range(-1000,80000,30):
+    altitude_data.append(h)
+    pressure_data.append(standardatmo.getPressureData(h))
+    temperature_data.append(standardatmo.getTemperatureData(h))
+    gravity_data.append(standardatmo.getGravityData(h))
+    density_data.append(standardatmo.getDensityData(h))
+    viscosity_data.append(standardatmo.getViscosityData(h))
+
+plt.plot(altitude_data, pressure_data, label = 'pressure')
+plt.plot(altitude_data, temperature_data, label = 'temperature')
+plt.plot(altitude_data, gravity_data, label = 'gravity')
+plt.plot(altitude_data, density_data, label = 'density')
+plt.plot(altitude_data, viscosity_data, label = 'viscosity')
+
+plt.legend()
+plt.show()
